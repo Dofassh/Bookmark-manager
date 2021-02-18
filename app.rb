c925b43a9c9ce03a32a631dd9a4b1e1beb2daebc
Dofassh <parmeetsembhi98@gmail.com>
2021-02-18 15:39:08 +0000

    challenge 10 complete

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
-require 'sinatra/base'
 require './lib/bookmark'
+require 'sinatra/base'
+
 
 class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
+    p ENV
+
     @bookmarks = Bookmark.all
-   erb :'bookmarks/index'
+    erb :'bookmarks/index'
   end
 
+  get '/bookmarks/new' do
+    erb :"/bookmarks/new"
+  end
 
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+    redirect '/bookmarks'
+  end
 
   run! if app_file == $0
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,13 +1,27 @@
 require 'bookmark'
 
 describe Bookmark do
-    describe '.all' do
-      it 'returns all the bookmarks' do
-        bookmarks = Bookmark.all
+  describe '.all' do
+    it 'returns all the bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+        # Add the test data
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      bookmarks = Bookmark.all
         
-        expect(bookmarks).to include("http://www.makersacademy.com")
-        expect(bookmarks).to include("http://www.destroyallsoftware.com")
-        expect(bookmarks).to include("http://www.google.com")
-      end
+      expect(bookmarks).to include("http://www.makersacademy.com")
+      expect(bookmarks).to include("http://www.destroyallsoftware.com")
+      expect(bookmarks).to include("http://www.google.com")
     end
+  end
+
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+    end
+  end
+
 end
